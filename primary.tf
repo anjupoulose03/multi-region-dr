e265f5e55a5dfc3d4dbde9f689f8526436aae60d
Anju Poulose <159629591+anjupoulose03@users.noreply.github.com>
2025-09-08 09:04:31 -0400

    Update primary.tf

--- primary.tf
+++ primary.tf
@@ -3,9 +3,9 @@ module "vpc_primary" {
   source               = "./modules/vpc"
   name_prefix          = "primary"
   vpc_cidr             = "10.0.0.0/16"
-  public_subnet_cidrs  = ["10.0.1.0/24"]
-  private_subnet_cidrs = ["10.0.2.0/24"]
-  azs                  = ["us-east-2a"]
+  public_subnet_cidrs  = ["10.0.1.0/24", "10.0.3.0/24"]
+  private_subnet_cidrs = ["10.0.2.0/24", "10.0.4.0/24"]
+  azs                  = ["us-east-2a", "us-east-2b"]
 }
 
 # EC2 Instance in Primary Region
