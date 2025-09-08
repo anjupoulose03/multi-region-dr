e1f0250c922ddb9d02ac646ac39ce53319ea6e85
Anju Poulose <159629591+anjupoulose03@users.noreply.github.com>
2025-09-08 09:05:22 -0400

    Update secondary.tf

--- secondary.tf
+++ secondary.tf
@@ -7,8 +7,8 @@ module "vpc_secondary" {
   name_prefix          = "secondary"
   vpc_cidr             = "10.1.0.0/16"
   public_subnet_cidrs  = ["10.1.1.0/24", "10.1.2.0/24"]
-  private_subnet_cidrs = ["10.1.3.0/24"]
-  azs                  = ["us-west-1a", "us-west-1b"]
+  private_subnet_cidrs = ["10.1.3.0/24", "10.1.4.0/24"]
+  azs                  = ["us-west-1a", "us-west-1c"]
 }
 
 module "ec2_secondary" {
