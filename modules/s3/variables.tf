d558bcdaa8652713c119077e78338c6070da2da4
Anju Poulose <159629591+anjupoulose03@users.noreply.github.com>
2025-09-09 20:55:30 -0400

    Update variables.tf

--- modules/s3/variables.tf
+++ modules/s3/variables.tf
@@ -1,14 +1,33 @@
 variable "name_prefix" {
-  description = "Prefix for the S3 bucket name"
+  description = "Base name, e.g. primary-backup-bucket-<app-id> (module appends -backup-bucket-<region>)."
   type        = string
 }
 
 variable "region" {
-  description = "AWS region"
+  description = "Region string (e.g., us-east-2 or us-west-1) appended to the bucket name."
   type        = string
 }
 
-variable "replication_arn" {
-  description = "ARN of the destination bucket for replication"
+variable "enable_replication" {
+  description = "Enable CRR from this (source) bucket."
+  type        = bool
+  default     = false
+}
+
+variable "replication_destination_bucket_arn" {
+  description = "Destination bucket ARN (arn:aws:s3:::<bucket-name>) when enable_replication=true."
   type        = string
+  default     = null
+}
+
+variable "replication_role_arn" {
+  description = "IAM role ARN used by S3 replication when enable_replication=true."
+  type        = string
+  default     = null
+}
+
+variable "tags" {
+  description = "Extra tags."
+  type        = map(string)
+  default     = {}
 }
